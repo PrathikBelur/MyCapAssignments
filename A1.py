27783d49d17c4802da0e947a13131f23387193a0
PrathikBelur <106919286+PrathikBelur@users.noreply.github.com>
2022-06-05 20:13:02 +0530

    Create A1.py

---
+++ A1.py
@@ -0,0 +1,10 @@
+#area of the circle
+r = float(input("enter radius of the circle:"))
+a = 3.14 * r * r
+print("area of circle with radius"r":",a)
+
+
+#file with python extension
+fn=input("enter the file name:")
+ext=fn+(".python")
+print("file with python extension:",ext)
